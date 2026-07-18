1ad5997e4cd966c2eb3d6cba0ade76104e3614d5
k5situ <k5situ@uwaterloo.ca>
2026-07-18 12:35:33 -0400

    add

--- card-impl.cc
+++ card-impl.cc
@@ -2,5 +2,5 @@ module card;
 
 void Subject::attach(Observer *o) { observers.emplace_back(o); }
 void Subject::notifyObservers() { for (auto &ob : observers) ob->notify(*this); }
-void Subject::setState(State newS) { state = newS; }
-State Subject::getState() const { return state; }
+//void Subject::setState(State newS) { state = newS; }
+//State Subject::getState() const { return state; }
