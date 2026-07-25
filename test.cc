05ec9c92f4f23b8ff199b5f9bd69ebecd64743da
k5situ <k5situ@uwaterloo.ca>
2026-07-25 12:44:42 -0400

    test

---
+++ test.cc
@@ -0,0 +1,6 @@
+std::tuple<std::string, int, std::string desc> spellTuple{"Banish", ,"Destroy target minion or ritual"};
+std::tuple<std::string, int, std::string desc> spellTuple{"Unsummon", , "Return target minion to its owner's hand"};
+std::tuple<std::string, int, std::string desc> spellTuple{"Recharge", , "Your ritual gains 3 charges"};
+std::tuple<std::string, int, std::string desc> spellTuple{"Disenchant", , "Destroy the top enchantment"};
+std::tuple<std::string, int, std::string desc> spellTuple{"Raise Dead", , "Resurrect the top minion in your graveyard and set its defence to 1"};
+std::tuple<std::string, int, std::string desc> spellTuple{"Blizzard", , "Deal 2 damage to all minions"};
