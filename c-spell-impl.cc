e2a9f4d87094e25fe932e970bd28bd9a0b54a751
k5situ <k5situ@uwaterloo.ca>
2026-07-25 23:47:08 -0400

    save

--- c-spell-impl.cc
+++ c-spell-impl.cc
@@ -35,21 +35,18 @@ Spell::Spell(string name, int cost): Card{name, cost} {
 
 void Spell::banish(Player *played, int indexC, Player *owner, int i) {
   cout << "banish called" << endl;
-  if ((owner) && (owner->getSizeH() > i)) {
-  // Minion *temp = dynamic_cast<Minion*>(owner->getCardH(i));
-  // temp->addA(2);
-  // temp->addD(2);
+  if ((owner) && (owner->getSizeB() > i)) {
+  owner->removeFromBoard(i);
   } else cout << "bad index" << endl;
-    // Card *it = owner->board[i];
-    // owner->board.erase(it);
 }
+
 void Spell::unsummon(Player *played, int indexC, Player *owner, int i) {
   cout << "unsummon called" << endl;
     // owner->hand.emplace_back = owner->board[i]; //add to hand
     // Card *it = owner->getCardB();
     // owner->board.erase(it); //remove from board
-
 }
+
 void Spell::recharge(Player *played, int indexC) {
   cout << "recharge called" << endl;
     // int len = played->board.size;
